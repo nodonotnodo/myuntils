7844eff8cce770068f620c119434dda3f8759ce8
nodonotnodo <18292026871@163.com>
2018-12-07 17:38:59 +0800

    工具类实现

--- src/main/java/com/nodonotnodo/sort/impl/BubbleSorter.java
+++ src/main/java/com/nodonotnodo/sort/impl/BubbleSorter.java
@@ -11,23 +11,67 @@ import com.nodonotnodo.sort.Sorter;
  */
 public class BubbleSorter implements Sorter {
     
+//    @Override
+//    public void sort(Element[] elements) {
+//        for (int i = 0; i < elements.length - 1; i++) {
+//            boolean flag = false;
+//            //如果j索引处的元素大于j+1索引处的元素，进行交换
+//            for (int j = 0; j < elements.length - 1 - i; j++) {
+//                if (elements[j].compareTo(elements[j + 1]) > 0) {
+//                    Element temp = elements[j];
+//                    elements[j] = elements[j + 1];
+//                    elements[j + 1] = temp;
+//                    flag = true;
+//                }
+//            }
+//            //如果某趟没有发生交换，则表明已处于有序状态
+//            if (!flag) {
+//                break;
+//            }
+//        }
+//    }
+
+
     @Override
-    public void sort(Element[] elements) {
-        for (int i = 0; i < elements.length - 1; i++) {
+    public void sort(Element[] elements){
+        for(int i = 0; i<elements.length-1; i++){
             boolean flag = false;
-            //如果j索引处的元素大于j+1索引处的元素，进行交换
-            for (int j = 0; j < elements.length - 1 - i; j++) {
-                if (elements[j].compareTo(elements[j + 1]) > 0) {
-                    Element temp = elements[j];
-                    elements[j] = elements[j + 1];
-                    elements[j + 1] = temp;
+            for(int j = 0; j<elements.length-1-i; j++){
+                //元素比较，升序排序
+                if(elements[j].compareTo(elements[j+1])>0){
+                    Element tmp = elements[j];
+                    elements[j] = elements[j+1];
+                    elements[j+1] = tmp;
                     flag = true;
                 }
             }
-            //如果某趟没有发生交换，则表明已处于有序状态
-            if (!flag) {
+            //此时数组已经有序
+            if(!flag){
                 break;
             }
         }
     }
+
+//    public static void print(Element[] elements){
+//        for(Element v:elements){
+//            System.out.println("key:"+v.getKey()+"\tdata:"+v.getData());
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Element[] test = new Element[5];
+//        test[0] = new Element(0,23);
+//        test[1] = new Element(3,22);
+//        test[2] = new Element(2,1);
+//        test[3] = new Element(6,34);
+//        test[4] = new Element(4,2);
+//        print(test);
+//        BubbleSorter b = new BubbleSorter();
+//        b.sort(test);
+//        System.out.println("排序后：");
+//        print(test);
+//
+//    }
+
+
 }
--- src/main/java/com/nodonotnodo/sort/impl/HeapSorter.java
+++ src/main/java/com/nodonotnodo/sort/impl/HeapSorter.java
@@ -10,9 +10,62 @@ import com.nodonotnodo.sort.Sorter;
  * Created: 2018/10/13
  */
 public class HeapSorter implements Sorter {
-    
+
     @Override
     public void sort(Element[] elements) {
-        //TODO
+        //升序所以建大堆
+        int size = elements.length;
+        while(size>0){
+            //建大堆
+            for(int i = size/2-1; i>=0; i--){
+                int root = i;
+                while(root*2+1<size){
+                    int left = root*2+1;
+                    int maxChild = left;
+                    if(root*2+2<=size-1 && elements[root*2+2].compareTo(elements[root*2+1])>0){
+                        maxChild = root*2+2;
+                    }
+                    if(elements[maxChild].compareTo(elements[root])>0){
+                        Element tmp = elements[maxChild];
+                        elements[maxChild] = elements[root];
+                        elements[root] = tmp;
+                        root = maxChild;
+                        continue;
+                    }
+                    break;
+                }
+            }
+            //将大堆的堆顶元素删除
+            Element tmp = elements[0];
+            elements[0] = elements[size-1];
+            elements[size-1] = tmp;
+            size--;
+        }
     }
+
+//    public static void print(Element[] elements){
+//        for(Element v:elements){
+//            System.out.println("key:"+v.getKey()+"\tdata:"+v.getData());
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        HeapSorter h = new HeapSorter();
+//        Element[] test= new Element[8];
+//        test[0] = new Element(23,0);
+//        test[1] = new Element(34,0);
+//        test[2] = new Element(64,0);
+//        test[3] = new Element(12,0);
+//        test[4] = new Element(9,0);
+//        test[5] = new Element(9,0);
+//        test[6] = new Element(0,0);
+//        test[7] = new Element(-1,0);
+//        print(test);
+//        System.out.println("堆排序后：");
+//        h.sort(test);
+//        print(test);
+//    }
+
+
+
 }
