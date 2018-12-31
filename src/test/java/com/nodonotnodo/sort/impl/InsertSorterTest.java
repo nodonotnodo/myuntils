a4eb7d6b17e5b9c0a5126411b45549a4a5ccdf5b
nodonotnodo <18292026871@163.com>
2018-12-31 17:42:27 +0800

    插排的测试

--- src/test/java/com/nodonotnodo/sort/impl/InsertSorterTest.java
+++ src/test/java/com/nodonotnodo/sort/impl/InsertSorterTest.java
@@ -6,12 +6,14 @@ public class InsertSorterTest {
 
     public static void main(String[] args) {
         InsertSorter i = new InsertSorter();
-        Element[] test = new Element[5];
+        Element[] test = new Element[7];
         test[0] = new Element(55,0);
         test[1] = new Element(44,0);
         test[2] = new Element(23,0);
         test[3] = new Element(35,0);
         test[4] = new Element(12,0);
+        test[5] = new Element(29,0);
+        test[6] = new Element(98,0);
         Print.print(test);
         System.out.println("排序后：");
         i.sort(test);
